d00832c78913d95a0d3d85e2092666c3b37133ef
Maruf Rahman <marufmunem@gmail.com>
2025-09-29 10:08:24 +0600

    footer component added

--- README.md
+++ README.md
@@ -2,8 +2,9 @@
 
 Tools used: React (with Vite), Tailwind CSS, Appwrite (Backend), TheMovieDB (API)
 
-🌐http://marufrahman.dev.movieapp.cartmixbd.com/
+🌐http://movieapp.dev.cartmixbd.com/
 
 ### Key Features
+
 - Search optimization (debounce)
 - Movie trendlist based on user search count (appwrite)
---
+++ src/components/Footer.jsx
@@ -0,0 +1,27 @@
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <div className="footer-content">
+        <div>Developed by Maruf Rahman</div>
+        <div>
+          <a
+            href="https://www.linkedin.com/in/marufrahmanpro"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            LinkedIn
+          </a>
+        </div>
+        <div>
+          <a
+            href="https://www.youtube.com/@marufrahmandigital"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            YouTube
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
+}
